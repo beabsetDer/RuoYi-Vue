05b59c3ef240ebc5131329cc672a247f0bab936c
Faker <Faker@work>
2022-03-17 14:04:09 +0800

    允許下載匿名訪問

--- ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/SecurityConfig.java
@@ -20,7 +20,7 @@ import com.ruoyi.framework.security.handle.LogoutSuccessHandlerImpl;
 
 /**
  * spring security配置
- * 
+ *
  * @author ruoyi
  */
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
@@ -92,7 +92,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.permitAll().antMatchers("/swagger-ui.html").anonymous().antMatchers("/swagger-resources/**")
 				.anonymous().antMatchers("/webjars/**").anonymous().antMatchers("/*/api-docs").anonymous()
 				.antMatchers("/druid/**").anonymous()
-
+				.antMatchers("/ks/employee/exportExcel/**").permitAll()
 				// 除上面外的所有请求全部需要鉴权认证
 				.anyRequest().authenticated().and().headers().frameOptions().disable();
 		httpSecurity.logout().logoutUrl("/logout").logoutSuccessHandler(logoutSuccessHandler);
